34b4026fc88c80cc197e69a8b5a81ea31c660d41
joerg.krause <joerg@krause.net>
2018-03-08 12:14:11 +0100

    Some refiners

--- WebFrontEndServer/WebFrontEnd/gulpfile.js
+++ WebFrontEndServer/WebFrontEnd/gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require('gulp');
 
-gulp.task('copy', function() {
+gulp.task('copy:css', function() {
     // Glob ** *. !*.*test*.
     return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css')
             .pipe(gulp.dest('./wwwroot/css'));
 
 });
 
-gulp.task('default', ['copy']);
+gulp.task('copy:js', function() {
+    // Glob ** *. !*.*test*.
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.css',
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './node_modules/bootstrap/dist/js/bootstrap.css',
+    ]).pipe(gulp.dest('./wwwroot/css'));
+
+});
+
+gulp.task('default', ['copy:css', 'copy:js']);
